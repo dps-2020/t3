7904dd83f4a45c12e739c00b518e2be14049c923
dlasecki <dlasecki@dlasecki-wrkstn>
2017-09-27 21:09:03 -0400

    Initial conditions for test were not created

--- src/junit/AccountOwnerMultipleTest.java
+++ src/junit/AccountOwnerMultipleTest.java
@@ -7,29 +7,30 @@ import org.junit.Before;
 import org.junit.Test;
 
 import banksystem.AccountOwner;
+import database.Database;
 
 public class AccountOwnerMultipleTest {
+	Database dataBase = Database.getInstance();
 
 	@Before
 	public void setUp() throws Exception {
+		Database.setFileName("test.dat");
+		dataBase.eraseFile();
+		dataBase.load();
 	}
 
 	@Test
-	public void test() {
+	public void createTwoAccounts() {
 		AccountOwner ownerOne = new AccountOwner("ownerOne", "J$");
 		ownerOne.put();
+		Assert.assertEquals(ownerOne.getId(), "O1001");
 
-		String ownerIdOne = ownerOne.getId();
-		Assert.assertEquals(ownerIdOne, "O1001");
+		AccountOwner ownerOneWrittenToDatabase = AccountOwner.get(ownerOne.getId());
 
-		AccountOwner ownerOneWrittenToDatabase = AccountOwner.get(ownerIdOne);
-
-		Assert.assertEquals(ownerIdOne, ownerOneWrittenToDatabase.getId());
+		Assert.assertEquals(ownerOne.getId(), ownerOneWrittenToDatabase.getId());
 		Assert.assertEquals("ownerOne", ownerOneWrittenToDatabase.getName());
 		Assert.assertEquals("J$", ownerOneWrittenToDatabase.getPassword());
-		
-		
-		
+
 		AccountOwner ownerTwo = new AccountOwner("ownerTwo", "J$");
 		ownerTwo.put();
 
@@ -41,7 +42,7 @@ public class AccountOwnerMultipleTest {
 		Assert.assertEquals(ownerIdTwo, ownerTwoWrittenToDatabase.getId());
 		Assert.assertEquals("ownerTwo", ownerTwoWrittenToDatabase.getName());
 		Assert.assertEquals("J$", ownerTwoWrittenToDatabase.getPassword());
-		
+
 	}
 
 }
